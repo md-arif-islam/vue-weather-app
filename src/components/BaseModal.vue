e51ba5d88b5129438ae182d4a692ca6c7499ab02
md-arif-islam <arifislamarif344@gmail.com>
2023-08-27 19:38:11 +0600

    UPDATED: BaseModal.vue
    - Updated the transition properties for the outer modal
    - Updated the transition properties for the inner modal
    - Added transition effects for the enter and leave states of both modals
    - Added opacity and transform properties for the enter and leave states of the inner modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,21 +1,25 @@
 <template>
-  <div
-    v-if="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-  >
+  <Transition name="modal-outer">
     <div
-      v-if="modalActive"
-      class="p-4 bg-white self-start mt-32 max-w-screen-md"
+      v-show="modalActive"
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
     >
-      <slot />
-      <button
-        @click="$emit('close-modal')"
-        class="text-w0hite mt-8 bg-weather-primary py-2 px-6"
-      >
-        Close
-      </button>
+      <Transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="p-4 bg-white self-start mt-32 max-w-screen-md"
+        >
+          <slot />
+          <button
+            @click="$emit('close-modal')"
+            class="text-w0hite mt-8 bg-weather-primary py-2 px-6"
+          >
+            Close
+          </button>
+        </div>
+      </Transition>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -29,4 +33,31 @@ defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
