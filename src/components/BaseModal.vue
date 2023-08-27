774b31788cc32a2e62a0f1c7769ffe8301df5cb9
md-arif-islam <arifislamarif344@gmail.com>
2023-08-27 19:33:26 +0600

    ADDED: BaseModal component and functionality - Added BaseModal.vue file - Implemented modalActive prop to show/hide the modal - Added slot for content inside the modal - Added Close button to close the modal - Created script setup to define emits and props
    UPDATED: SiteNavigation.vue
    - Imported BaseModal component
    - Added toggleModal function to toggle the visibility of the modal
    - Added click event to the info icon to open the modal
    - Added BaseModal component with content in the modal

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    v-if="modalActive"
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+  >
+    <div
+      v-if="modalActive"
+      class="p-4 bg-white self-start mt-32 max-w-screen-md"
+    >
+      <slot />
+      <button
+        @click="$emit('close-modal')"
+        class="text-w0hite mt-8 bg-weather-primary py-2 px-6"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineEmits(["close-modal"]);
+
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
