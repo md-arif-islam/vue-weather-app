6437c07620dd8d62ce51dd46dc8544f779cd74ce
md-arif-islam <arifislamarif344@gmail.com>
2023-08-27 22:12:52 +0600

    ADDED: CityView component and route.
    - Created CityView.vue file.
    - Imported CityView component in router/index.js.
    - Added a new route for CityView.
    
    ADDED: Error handling for mapbox search.
    
    - Added searchError ref variable.
    - Handled errors in the getSearchResults function.
    - Displayed error messages on the HomeView template.
    
    UPDATED: previewCity function.
    
    - Modified previewCity function to use router.push with updated parameters and query strings.
    
    UPDATED: getSearchResults function.
    
    - Added a try-catch block to handle errors when making the API request.
    - Set the searchError ref to true in case of an error.
    
    UPDATED: HomeView template.
    
    - Added conditional rendering for error messages.
    - Wrapped the list items inside a <template> tag for conditional rendering.
    - Improved error messages to provide more information.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,14 +12,22 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="mapboxSearchResults"
       >
-        <li
-          v-for="searchResult in mapboxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-          @click="previewCity(searchResult)"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p class="py-2" v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -28,23 +36,42 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
 const mapboxAPIKey =
   "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const router = useRouter();
+
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-
-      console.log(mapboxSearchResults.value);
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
 
       return;
     }
@@ -53,4 +80,4 @@ const getSearchResults = () => {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
