ea432e539dbdcab5db4315da0c189de7d407632c
md-arif-islam <arifislamarif344@gmail.com>
2023-08-27 20:41:38 +0600

    UPDATED: Added axios as a dependency in package.json.
    - Added axios dependency with version 1.5.0 to package-lock.json.
    - Added asckit dependency with version 0.4.0 to package-lock.json.
    - Added combined-stream dependency with version 1.0.8 to package-lock.json.
    - Added axios dependency with version 1.5.0 to package-lock.json.
    - Added follow-redirects dependency with version 1.15.2 to package-lock.json.
    - Added form-data dependency with version 4.0.0 to package-lock.json.
    - Added proxy-from-env dependency with version 1.1.0 to package-lock.json.
    - Added delayed-stream dependency with version 1.0.0 to package-lock.json.
    - Added mime-db dependency with version 1.52.0 to package-lock.json.
    - Added mime-types dependency with version 2.1.35 to package-lock.json.
    - Updated code in BaseModal Teleport and provide better accessibility.
    - Updated code in HomeView.vue to include search functionality using Map.

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,25 +1,27 @@
 <template>
-  <Transition name="modal-outer">
-    <div
-      v-show="modalActive"
-      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-    >
-      <Transition name="modal-inner">
-        <div
-          v-if="modalActive"
-          class="p-4 bg-white self-start mt-32 max-w-screen-md"
-        >
-          <slot />
-          <button
-            @click="$emit('close-modal')"
-            class="text-w0hite mt-8 bg-weather-primary py-2 px-6"
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
           >
-            Close
-          </button>
-        </div>
-      </Transition>
-    </div>
-  </Transition>
+            <slot />
+            <button
+              @click="$emit('close-modal')"
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup>
